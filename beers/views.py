e8c910ee154887fcedc327942267544c3229594a
Andy Russell <andy.russell.code@gmail.com>
2018-02-08 05:47:03 -0600

    BeerDetail pages show user's rating or show drop down to rate.

--- beers/views.py
+++ beers/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
-from django.views import generic
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from django.views.generic.detail import SingleObjectMixin
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Brewery, Beer, Rating
+from django.views.generic.edit import FormMixin
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.db.models import Avg
+from .forms import RatingForm
+from .models import Brewery, Beer, Rating
+
+
 
 
 # Landing page with list of most recent beers and and invite to create user account
@@ -39,7 +43,7 @@ class BreweryDetailPage(SingleObjectMixin, ListView):
 
 
 # List of all breweries using generic included ListView
-class AllBreweries(generic.ListView):
+class AllBreweries(ListView):
     model = Brewery
     context_object_name = 'all_breweries'
     template_name = 'beers/breweries_list.html'
@@ -52,7 +56,7 @@ class AllBreweries(generic.ListView):
 
 
 # List of all Beers
-class AllBeers(generic.ListView):
+class AllBeers(ListView):
     model = Beer
     context_object_name = 'all_beers'
     template_name = 'beers/beer_list.html'
@@ -71,24 +75,52 @@ class AllBeers(generic.ListView):
         return context
 
 
-class BeerDetailView(generic.DetailView):
+class BeerDetailView(FormMixin, DetailView):
     model = Beer
+    form_class = RatingForm
+
+    def get_success_url(self):
+        return reverse('beer_detail', kwargs={'pk': self.object.id})
 
     def get_context_data(self, **kwargs):
         context = super(BeerDetailView, self).get_context_data(**kwargs)
-        context['all_ratings'] = Rating.objects.all()
+        context['form'] = RatingForm(initial={'post': self.object.id})
+        try:
+            rating = Rating.objects.get(rating_user=self.request.user, rating_beer=self.object).rating_score
+            context['current_rating'] = int(rating)
+            int_list = []
+
+            for i in range(context['current_rating']):
+                int_list.append('x')
+
+            context['star_rating'] = int_list
+
+        except ObjectDoesNotExist:
+            print('Beer not yet rated by the current user.')
+
         return context
 
-    def already_rated(self):
-
-
-
     def post(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.get_form()
+
+        if form.is_valid():
+            rating = Rating.objects.create(rating_beer=self.object, rating_user=self.request.user, rating_score=form.cleaned_data['rating_score'])
+            rating.save()
+            return super(BeerDetailView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
-class AddBeerView(generic.CreateView):
+
+
+        #if not request.user.is_authenticated:
+         #   return HttpResponseForbidden()
+
+
+
+
+class AddBeerView(CreateView):
     model = Beer
     fields = ['beer_name', 'beer_brewery', 'beer_style', 'beer_abv', 'beer_srm', 'beer_logo', 'beer_photo']
 
